6c47f22f340c85bf539b246f1829c5f4ec01d507
akira9889 <aki_badmin89@icloud.com>
2023-09-26 20:48:35 +0900

    開発イメージと本番イメージをまとめる

--- docker/start.sh
+++ docker/start.sh
@@ -2,26 +2,42 @@
 
 if [ "$APP_ENV" = "production" ]; then
   # 本番環境で実行するコマンド
+  echo "Set config"
+  echo DB_HOST=$DB_HOST >> .env.production &&
+  echo DB_DATABASE=$DB_DATABASE >> .env.production &&
+  echo DB_USERNAME=$DB_USERNAME >> .env.production &&
+  echo DB_PASSWORD=$DB_PASSWORD >> .env.production &&
+  echo APP_NAME=$APP_NAME >> .env.production &&
+  echo APP_KEY=$APP_KEY >> .env.production &&
+  echo APP_URL=$APP_URL >> .env.production &&
+  echo SESSION_DRIVER=$SESSION_DRIVER >> .env.production &&
+  echo SANCTUM_STATEFUL_DOMAINS=$SANCTUM_STATEFUL_DOMAINS >> .env.production &&
+  echo SESSION_DOMAIN=$SESSION_DOMAIN >> .env.production &&
+  echo 'Laravel env variables configured'
+  echo "Cache config"
   php artisan config:cache --env=production
+  echo "Migrate"
   php artisan migrate --force
   php-fpm
 elif [ "$APP_ENV" = "test" ]; then
   # テスト環境で実行するコマンド
+  echo "Set config"
+  echo DB_HOST=$DB_HOST >> .env.test &&
+  echo DB_DATABASE=$DB_DATABASE >> .env.test &&
+  echo DB_USERNAME=$DB_USERNAME >> .env.test &&
+  echo DB_PASSWORD=$DB_PASSWORD >> .env.test &&
+  echo APP_NAME=$APP_NAME >> .env.test &&
+  echo APP_KEY=$APP_KEY >> .env.test &&
+  echo APP_URL=$APP_URL >> .env.test &&
+  echo SESSION_DRIVER=$SESSION_DRIVER >> .env.test &&
+  echo SANCTUM_STATEFUL_DOMAINS=$SANCTUM_STATEFUL_DOMAINS >> .env.test &&
+  echo SESSION_DOMAIN=$SESSION_DOMAIN >> .env.test &&
+  echo 'Laravel env variables configured'
+  echo "Cache config"
   php artisan config:cache --env=test
+  echo "Migrate"
   php artisan migrate --force
   php-fpm
 else
-  # 本番環境、テスト環境以外で実行するコマンド
-  echo DB_HOST=$DB_HOST >> .env &&
-  echo DB_DATABASE=$DB_DATABASE >> .env &&
-  echo DB_USERNAME=$DB_USERNAME >> .env &&
-  echo DB_PASSWORD=$DB_PASSWORD >> .env &&
-  echo APP_NAME=$APP_NAME >> .env &&
-  echo APP_KEY=$APP_KEY >> .env &&
-  echo APP_URL=$APP_URL >> .env &&
-  echo SESSION_DRIVER=$SESSION_DRIVER >> .env &&
-  echo SANCTUM_STATEFUL_DOMAINS=$SANCTUM_STATEFUL_DOMAINS >> .env &&
-  echo SESSION_DOMAIN=$SESSION_DOMAIN >> .env &&
-  echo 'Laravel env variables configured'
   php-fpm
 fi
