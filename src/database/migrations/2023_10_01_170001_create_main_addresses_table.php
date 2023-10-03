dca3a0b633467881ef2ac88c4a793bff8aa399cd
akkie1010 <a7432794@gmail.com>
2023-10-03 12:24:05 +0900

    タイムスタンプを削除

--- src/database/migrations/2023_10_01_170001_create_main_addresses_table.php
+++ src/database/migrations/2023_10_01_170001_create_main_addresses_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('main_addresses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->timestamps();
         });
     }
 
