9270deded0821b4d9c7e6a946880d4e519347d55
akkie1010 <a7432794@gmail.com>
2023-10-08 08:33:52 +0900

    ボタンコンポーネントの追加

---
+++ src/resources/js/components/Btn.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  text: String,
+  type: String,
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+})
+
+const btnClasses = computed(() => {
+  const cls = [`mt-2 py-2 px-4 text-white rounded-3xl`]
+
+  if (props.disabled) {
+    cls.push('bg-red-100');
+  } else {
+    if (props.type === 'primary') {
+      cls.push(`bg-customBlue`)
+    } else if (props.type === 'danger') {
+      cls.push(`bg-customRed`)
+    } else {
+      cls.push(`bg-customGray`)
+    }
+  }
+
+  return cls.join(' ')
+})
+</script>
+
+<template>
+  <button :class="btnClasses" :disabled="disabled">{{ text }}</button>
+</template>
