aef21702da59f2955148f3a0e3f94747ec945a80
akkie1010 <a7432794@gmail.com>
2023-10-08 08:34:49 +0900

    InputErrorコンポーネントの追加

---
+++ src/resources/js/components/InputError.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  errorMsg: Object,
+})
+</script>
+
+<template>
+<ul v-if="errorMsg" class="text-red-500 text-sm leading-4">
+  <li v-for="(error, index) in errorMsg" :key="index"><p>{{ error }}</p></li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
