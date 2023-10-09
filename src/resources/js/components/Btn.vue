7d7ecf5701636257ebba86a21f1d92c70ed88dd7
akkie1010 <a7432794@gmail.com>
2023-10-10 00:12:49 +0900

    デザイン修正

--- src/resources/js/components/Btn.vue
+++ src/resources/js/components/Btn.vue
@@ -10,7 +10,7 @@ const props = defineProps({
 })
 
 const btnClasses = computed(() => {
-  const cls = [`mt-2 py-2 px-4 text-white rounded-3xl`]
+  const cls = [`py-2 px-4 text-white rounded-3xl`]
 
   if (props.disabled) {
     cls.push('bg-red-100')
