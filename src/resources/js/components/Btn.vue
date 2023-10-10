544f5ddd345f8c6cd91a97cc39c60f5a24421136
akira9889 <aki_badmin89@icloud.com>
2023-10-10 12:03:49 +0900

    プロップの修正

--- src/resources/js/components/Btn.vue
+++ src/resources/js/components/Btn.vue
@@ -7,21 +7,24 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  bgColor: String,
 })
 
 const btnClasses = computed(() => {
   const cls = [`py-2 px-4 text-white rounded-3xl`]
 
-  if (props.disabled) {
-    cls.push('bg-red-100')
-  } else {
-    if (props.type === 'primary') {
-      cls.push(`bg-customBlue`)
-    } else if (props.type === 'danger') {
-      cls.push(`bg-customRed`)
-    } else {
-      cls.push(`bg-customGray`)
+  if (props.bgColor === 'primary') {
+    cls.push(`bg-customBlue`)
+    if (props.disabled) {
+      cls.push('bg-sky-100')
     }
+  } else if (props.bgColor === 'danger') {
+    cls.push(`bg-customRed`)
+    if (props.disabled) {
+      cls.push('bg-red-100')
+    }
+  } else {
+    cls.push(`bg-customGray`)
   }
 
   return cls.join(' ')
