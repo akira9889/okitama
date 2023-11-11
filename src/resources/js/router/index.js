9056ac7408f6c2c54b6253c8e0e7d5c747711e99
akira9889 <aki_badmin89@icloud.com>
2023-11-12 01:29:15 +0900

    authミドルウェアを使用

--- src/resources/js/router/index.js
+++ src/resources/js/router/index.js
@@ -51,7 +51,7 @@ const routes = [
         path: 'users',
         name: 'users',
         component: Users,
-        meta: { middleware: [admin, approved] },
+        meta: { middleware: [auth, approved, admin] },
       },
       {
         path: 'awaiting-users',
