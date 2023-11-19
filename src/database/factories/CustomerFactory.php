e864065750719f183f5f1e5510a983e5c74fe018
akira9889 <aki_badmin89@icloud.com>
2023-11-19 15:34:22 +0900

    漢字とふりがなのカラムを追加

--- src/database/factories/CustomerFactory.php
+++ src/database/factories/CustomerFactory.php
@@ -16,14 +16,21 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
-        $first_name = mb_convert_kana(fake()->firstKanaName, 'c');
-        $last_name = mb_convert_kana(fake()->lastKanaName, 'c');
+        $last_name = fake()->lastName;
+        $first_name = fake()->firstName;
         $full_name = $last_name . $first_name;
+
+        $first_kana = mb_convert_kana(fake()->firstKanaName, 'c');
+        $last_kana = mb_convert_kana(fake()->lastKanaName, 'c');
+        $full_kana = $last_kana . $first_kana;
 
         return [
             'first_name' => $first_name,
             'last_name' => $last_name,
             'full_name' => $full_name,
+            'first_kana' => $first_kana,
+            'last_kana' => $last_kana,
+            'full_kana' => $full_kana,
             'town_id' => \App\Models\Town::inRandomOrder()->first()->id,
             'address_number' => implode('-', str_split(fake()->randomNumber(3, true))),
             'room_number' => fake()->optional(0.5)->buildingNumber(),
--- src/database/migrations/2023_10_01_170005_create_customers_table.php
+++ src/database/migrations/2023_10_01_170005_create_customers_table.php
@@ -13,15 +13,19 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('last_name')->nullable();
             $table->string('first_name')->nullable();
-            $table->string('last_name');
-            $table->string('full_name');
+            $table->string('full_name')->nullable();
+            $table->string('last_kana')->nullable();
+            $table->string('first_kana')->nullable();
+            $table->string('full_kana')->nullable();
             $table->foreignId('town_id')->constrained();
             $table->string('address_number')->nullable();
             $table->string('room_number')->nullable();
             $table->text('description')->nullable();
 
             $table->index(['full_name', 'town_id']);
+            $table->index(['full_kana', 'town_id']);
         });
     }
 
