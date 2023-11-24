69ffadc2f03cf36fa73b4819286fff3b377f20a7
akira9889 <aki_badmin89@icloud.com>
2023-11-24 20:55:32 +0900

    通知は３つまでになるように修正

--- src/resources/js/store/modules/Toast.js
+++ src/resources/js/store/modules/Toast.js
@@ -7,6 +7,9 @@ export const state = {
 export const mutations = {
   ADD_TOAST(state, toast) {
     state.toasts.push(toast)
+    if (state.toasts.length > 3) {
+      state.toasts.splice(0, 1)
+    }
   },
   REMOVE_TOAST(state, id) {
     state.toasts = state.toasts.filter((toast) => toast.id !== id)
